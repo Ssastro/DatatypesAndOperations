a79a7369a9b5d3313a5c0ededf717f9b58ebdd67
Ssastro <82648309+Ssastro@users.noreply.github.com>
2021-05-19 14:11:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+
+from typing import type_check_only
+
+
+print(' Hello,world!')
+print (1,2,3)
+print('Python can waltz!')
+
+waltz='one two three'
+waltz
+wincpy type_check_only
+
+
+
+
+

0d3175bdd1957adde5e3c1c172e8cb94093b19f0
Ssastro <82648309+Ssastro@users.noreply.github.com>
2021-05-24 15:47:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,73 +4,71 @@ __human_name__ = 'strings'
 
 # Add your code after this lineSportief2021!
 
-#opdracht1 deel 1
+#opdracht1 deel 1 checked
 
-scoorder1 = ' Marco Van Basten'
-scoorder0= ' Ruud Gullit'
+scoorder1='Marco van Basten'
+scoorder0='Ruud Gullit'
 
-
-#opdracht 2 deel 1
+#opdracht 2 deel 1 checked
 goal_0=32
 goal_1=54
 
 
-#opdracht 3 deel 1 # check 
-scorers=scoorder0 + str(goal_0), scoorder1 + str(goal_1)
+#opdracht 3 deel 1 # checked
+scorers=scoorder0 + " " + str(goal_0)+ "," + scoorder1 + " " + str(goal_1)
+print(scorers)
 
+#opdracht 4 deel 1 #dchecked
 
-#opdracht 4 deel 1 #deze gecheckt
-
-second ='nd'
-third ='th'
-Report =f'{scoorder0}' + ' scored in the ' +f'{goal_0}'  + f'{second}' + ' minute ' '\n'+f'{scoorder1}' + ' scored in the ' +f'{goal_1}' +f'{third}' + ' minute'
-
-  
-
-
-
+Report =f'{scoorder0} scored in the {goal_0}nd minute \n'f' {scoorder1}  scored in the {goal_1}th minute'
+print(Report)
 
 #part 2
 
-#opdracht 1 deel 2
-player= 'Ruud Gullit' 
+#opdracht 1 deel 2 checked
+player='Ruud Gullit' 
 
-#opdracht 2 deel 2
-
-#find
-mystring="Ruud Gullit"
-print(mystring.find("Ruud"))
+#opdracht 2 deel 2 checked
 
 #slicing check
-first_name=player[0:5]
+first_name=player[0: player.find(' ')]
+print(first_name)
+
+#slicing check andere manier
+first_name=player[:player.find(' ')]
+print(first_name)
+
+#len
+first_name_len=(len(first_name))
+print(first_name_len)
 
 #opdracht 3 deel 2 check!!!!
 
-#find
-mystring="Ruud Gullit"
-print("The position of Gullit using find()is at:", mystring.find("Gullit"))
+last_name=player[player.find(' ')+1:]
+print(last_name)
 
 #len
-last_name_len='Gullit'
-print(last_name_len,len(last_name_len))
+last_name_len=(len(last_name))
+print(last_name_len)
 
-#slicing
-last_name=player[6:]
+player='Ruud Gullit'
+last_name=player[player.find(' ')]
 print(last_name)
+
 
 #opdracht 4 deel 2 
 
-name_short=((first_name[0:5])+ ' '+ (last_name))
+name_short=((first_name[0])+'.'+''+(last_name))
 print(name_short)
 
 
-#opdracht 5 deel 2 
-chant= ((first_name) + '!' + ' ') *(len(first_name))
+#opdracht 5 deel 2 checked
+chant= (((first_name) + '!' + ' ') *(len(first_name))).strip()
+print(chant)
 
 
 #opdracht 6 
-good_chant= (' '!=chant.strip()[-1])
+good_chant= ' '!=chant[-1]
 print(good_chant)
-
 
 
